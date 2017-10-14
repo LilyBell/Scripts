6c9f9192f3ae3d5543945ad40160058b6231c471
LilyBell <aurelian.design.company@gmail.com>
2017-10-14 10:33:29 -0400

    lbcreateapp completed

--- lbcreateapp.ps1
+++ lbcreateapp.ps1
@@ -9,41 +9,41 @@ $Repo = Read-Host -Prompt 'Input the git repo to be cloned'
 Write-Host "Creating New Project: '$ProjectName' Progress:[--------------------------------------------------] 0%"
 Write-Host "Cloning '$Repo' into '$DirectoryPath'\'$ProjectName'"
 Git clone -q $Repo .
-Write-Host "Creating New Project: '$ProjectName' Progress:[####----------------------------------------------] 8%"
+Write-Host "Creating New Project: '$ProjectName' Progress:[##------------------------------------------------] 5%"
 Write-Host "Removing .git"
 Rimraf .git
-Write-Host "Creating New Project: '$ProjectName' Progress:[########------------------------------------------] 16%"
+Write-Host "Creating New Project: '$ProjectName' Progress:[#####---------------------------------------------] 11%"
 Write-Host "Installing Dependencies"
 Yarn install --silent
-Write-Host "Creating New Project: '$ProjectName' Progress:[########------------------------------------------] 17%"
+Write-Host "Creating New Project: '$ProjectName' Progress:[########------------------------------------------] 16%"
 Write-Host "Creating Root Directories"
 $Invocation = Get-Location
 $DirectoryPath = $Invocation.Path
-Write-Host "Creating New Project: '$ProjectName' Progress:[#########-----------------------------------------] 18%"
+Write-Host "Creating New Project: '$ProjectName' Progress:[##########----------------------------------------] 21%"
 Write-Host "Creating src Directory"
 New-Item -Path $DirectoryPath -Name "src" -ItemType "directory"
-Write-Host "Creating New Project: '$ProjectName' Progress:[#########-----------------------------------------] 19%"
+Write-Host "Creating New Project: '$ProjectName' Progress:[#############-------------------------------------] 26%"
 Write-Host "Creating .vscode Directory"
 New-Item -Path $DirectoryPath -Name ".vscode" -ItemType "directory"
-Write-Host "Creating New Project: '$ProjectName' Progress:[##########----------------------------------------] 20%"
+Write-Host "Creating New Project: '$ProjectName' Progress:[#################---------------------------------] 32%"
 Write-Host "Creating .nuget Directory"
 New-Item -Path $DirectoryPath -Name ".nuget" -ItemType "directory"
-Write-Host "Creating New Project: '$ProjectName' Progress:[##########----------------------------------------] 21%"
+Write-Host "Creating New Project: '$ProjectName' Progress:[##################--------------------------------] 36%"
 Write-Host "Creating .blueprints Directory"
 New-Item -Path $DirectoryPath -Name ".blueprints" -ItemType "directory"
-Write-Host "Creating New Project: '$ProjectName' Progress:[############--------------------------------------] 25%"
+Write-Host "Creating New Project: '$ProjectName' Progress:[#####################-----------------------------] 42%"
 Write-Host "Creating .NET Solution"
 Dotnet new sln -n . -o . 
-Write-Host "Creating New Project: '$ProjectName' Progress:[############--------------------------------------] 25%"
+Write-Host "Creating New Project: '$ProjectName' Progress:[########################--------------------------] 47%"
 Write-Host "Correcting Solution Filename"
 Mv ..sln .sln
-Write-Host "Creating New Project: '$ProjectName' Progress:[############--------------------------------------] 25%"
+Write-Host "Creating New Project: '$ProjectName' Progress:[#############################---------------------] 58%"
 Write-Host "Creating .NET Core class library"
 Dotnet new classlib -n common -o ./src/common
-Write-Host "Creating New Project: '$ProjectName' Progress:[############--------------------------------------] 25%"
+Write-Host "Creating New Project: '$ProjectName' Progress:[###############################-------------------] 63%"
 Write-Host "Creating .NET Core ASP.NET project"
 Dotnet new mvc -n client -o ./src/client
-Write-Host "Creating New Project: '$ProjectName' Progress:[############--------------------------------------] 25%"
+Write-Host "Creating New Project: '$ProjectName' Progress:[###################################---------------] 68%"
 Write-Host "Adding projects to solution"
 Dotnet sln . add ./src/common/common.csproj
 Dotnet sln . add ./src/client/client.csproj
@@ -51,7 +51,7 @@ function OptionalBarebones () {
     $Optional = Read-Host -Prompt "Would you like to remove default views and controllers? [Y] Yes or [N] No"
     switch ($Optional) {
         Y { 
-            Write-Host "Creating New Project: '$ProjectName' Progress:[############--------------------------------------] 25%"
+            Write-Host "Creating New Project: '$ProjectName' Progress:[######################################-------------] 74%"
             Write-Host "Removing default views and controllers"
             mv ./src/client/appsettings.Development.json ./src/client/appsettings.dev.json
             rimraf ./src/client/wwwroot
@@ -63,7 +63,7 @@ function OptionalBarebones () {
             rimraf ./src/client/bundleconfig.json
          }
         N { 
-            Write-Host "Creating New Project: '$ProjectName' Progress:[############--------------------------------------] 25%"
+            Write-Host "Creating New Project: '$ProjectName' Progress:[######################################----------] 79%"
             Write-Host "Retaining default views and controllers"
          }
         Default {
@@ -75,12 +75,25 @@ function OptionalBarebones () {
 
 OptionalBarebones
 
-Write-Host "Creating New Project: '$ProjectName' Progress:[############--------------------------------------] 25%"
+Write-Host "Creating New Project: '$ProjectName' Progress:[##########################################---------] 84%"
 Write-Host "Moving Angular compatible views and controllers"
 Mv ./templates/Views ./src/client
 Mv ./templates/Controllers ./src/client
 Set-Location -Path ./src/client
-
-
+Write-Host "Creating New Project: '$ProjectName' Progress:[############################################-------] 89%"
+Write-Host "Creating Angular Project"
+Ng new wwwroot
+Set-Location -Path ./wwwroot
+Write-Host "Creating New Project: '$ProjectName' Progress:[###############################################---] 94%"
+Write-Host "Building Angular Project"
+Ng build
+$Invocation = Get-Location
+$Source = $Invocation.Path
+$Destination = Split-Path -Path $Source -Parent
+Set-Location -Path $Destination
+Write-Host "Creating New Project: '$ProjectName' Progress:[##################################################] 100%"
+Write-Host "Running .NET Project"
+Dotnet restore
+Dotnet run
 
 
--- readme.md
+++ readme.md
@@ -1,5 +1,8 @@
 All files contained within this repo are for the purpose of automating various project related tasks in Windows 10
 
 ## lbcreateapp
-### Status: Incomplete
+### Status: v 1.0.0
 The purpose of this script is to automate the creation of an Angular / .NET Core solution
+Manual solution creation instructions can be found [here](https://www.github.com/LilyBell/angular_dotnet_startup).
+
+
